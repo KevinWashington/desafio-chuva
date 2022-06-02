40932e22af503f0397182cfab79279c4ad26e6f0
Kevin Washington <87237986+KevinWashington@users.noreply.github.com>
2022-06-02 09:36:28 -0300

    Delete texteditor.js

--- texteditor.js
+++
@@ -1,9 +0,0 @@
-function bold() {
-    //function to make the text bold using DOM method
-    document.getElementById("textarea1").style.fontWeight = "bold";
-}
-  
-function italic() {
-    //function to make the text italic using DOM method
-    document.getElementById("textarea1").style.fontStyle = "italic";
-}
